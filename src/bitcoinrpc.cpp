52e520ff105c45e0fba052a892b1fde62494a130
Ruckard Smith <ruckard.smith@gmail.com>
2022-11-28 22:17:16 +0100

    FORK-WHITEX11: Port change.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 24071 : 15815;
+    return GetBoolArg("-testnet", false) ? 22347 : 32347;
 }
 
 Object JSONRPCError(int code, const string& message)
